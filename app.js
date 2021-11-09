c88541b6df109fca171142aa575712dc2203d31f
Andrii Lazariev <alazariev@lohika.com>
2021-11-09 13:09:06 +0200

    feat(hw-1): update after review

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const config = require('./config');
 const logger = require('./logger');
 
 http.createServer((req, res) => {
-    console.log('New incoming request');
+    logger.info('New incoming request');
     res.writeHeader(200, {'Content-Type': 'application/json'});
     res.end(JSON.stringify({ message: 'Hello world!'}));
 }).listen(config.APP_PORT | 3000, () => logger.info(`Server is listening on port ${ config.APP_PORT }. Env is ${ config.ENV }`))
